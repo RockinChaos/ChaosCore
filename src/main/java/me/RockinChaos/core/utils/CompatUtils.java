0845e9ee24d10d7fd25209a346dbd8ddd0c682b1
RockinChaos <JacobGaming@CraftationGaming.com>
2024-07-02 00:54:04 -0700

    chore: added compatibility handler
    Added a handler for bridging gaps between older and newer Minecraft versions. Allowing plugins to be complied against the latest official release.

---
+++ src/main/java/me/RockinChaos/core/utils/CompatUtils.java
@@ -0,0 +1,237 @@
+/*
+ * ChaosCore
+ * Copyright (C) CraftationGaming <https://www.craftationgaming.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package me.RockinChaos.core.utils;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+import java.lang.reflect.Method;
+
+/**
+ * Welcome to the magical land of hopes and dreams.
+ * These methods exist to bridge the gap between newer and older Minecraft versions
+ * with the goal to be able to compile using the latest Minecraft version.
+ */
+public class CompatUtils {
+
+
+    /**
+     * Attempts to get the Open Inventory of the Player.
+     * In API versions 1.21 (and above), InventoryView is an interface.
+     * In versions 1.20.6 and below, InventoryView is a class.
+     *
+     * @param object The object being referenced, this is a Player object.
+     * @return The open inventory view from the object, this is an InventoryView so only an object will be returned.
+     */
+    public static @Nonnull Object getOpenInventory(final @Nonnull Object object) {
+        try {
+            final Method getOpenInventory = object.getClass().getMethod("getOpenInventory");
+            getOpenInventory.setAccessible(true);
+            return getOpenInventory.invoke(object);
+        } catch (Exception e) {
+            ServerUtils.logSevere("{CompatUtils} An error has occurred with Player#getOpenInventory!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Attempts to get the Top Inventory of the Player, InventoryView, or InventoryEvent.
+     * In API versions 1.21 (and above), InventoryView is an interface.
+     * In versions 1.20.6 and below, InventoryView is a class.
+     *
+     * @param object The object being referenced, this can be either a Player object,InventoryView object, or InventoryEvent object.
+     * @return The top Inventory object from the object.
+     */
+    public static @Nonnull Inventory getTopInventory(final @Nonnull Object object) {
+        try {
+            final Object view = (object instanceof Player ? getOpenInventory(object) : (StringUtils.containsIgnoreCase(object.getClass().getName(), "InventoryView", "Container") ? object : ((InventoryEvent)object).getView()));
+            final Method getTopInventory = view.getClass().getMethod("getTopInventory");
+            getTopInventory.setAccessible(true);
+            return (Inventory) getTopInventory.invoke(view);
+        } catch (Exception e) {
+            ServerUtils.logSevere("{CompatUtils} An error has occurred with InventoryView#getTopInventory!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Attempts to get the Bottom Inventory of the Player, InventoryView, or InventoryEvent.
+     * In API versions 1.21 (and above), InventoryView is an interface.
+     * In versions 1.20.6 and below, InventoryView is a class.
+     *
+     * @param object The object being referenced, this can be either a Player object, InventoryView object, or InventoryEvent object.
+     * @return The bottom Inventory object from the object.
+     */
+    public static @Nonnull Inventory getBottomInventory(final @Nonnull Object object) {
+        try {
+            final Object view = (object instanceof Player ? getOpenInventory(object) : (StringUtils.containsIgnoreCase(object.getClass().getName(), "InventoryView", "Container") ? object : ((InventoryEvent)object).getView()));
+            final Method getBottomInventory = view.getClass().getMethod("getBottomInventory");
+            getBottomInventory.setAccessible(true);
+            return (Inventory) getBottomInventory.invoke(view);
+        } catch (Exception e) {
+            ServerUtils.logSevere("{CompatUtils} An error has occurred with InventoryView#getBottomInventory!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Attempts to get the Player from the InventoryView or InventoryEvent.
+     * In API versions 1.21 (and above), InventoryView is an interface.
+     * In versions 1.20.6 and below, InventoryView is a class.
+     *
+     * @param object The object being referenced, this can be either a InventoryView object or InventoryEvent object.
+     * @return The Player object from the object.
+     */
+    public static @Nonnull Player getPlayer(final @Nonnull Object object) {
+        try {
+            final Object view = (StringUtils.containsIgnoreCase(object.getClass().getName(), "InventoryView", "Container") ? object : ((InventoryEvent)object).getView());
+            final Method getPlayer = view.getClass().getMethod("getPlayer");
+            getPlayer.setAccessible(true);
+            return (Player) getPlayer.invoke(view);
+        } catch (Exception e) {
+            ServerUtils.logSevere("{CompatUtils} An error has occurred with InventoryView#getPlayer!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Attempts to get the Title of the InventoryView.
+     * In API versions 1.21 (and above), InventoryView is an interface.
+     * In versions 1.20.6 and below, InventoryView is a class.
+     *
+     * @param object The object being referenced, this can be either a Player object, InventoryView object, or InventoryEvent object.
+     * @return The Title String from the object.
+     */
+    public static @Nonnull String getInventoryTitle(final @Nonnull Object object) {
+        try {
+            final Object view = (object instanceof Player ? getOpenInventory(object) : (StringUtils.containsIgnoreCase(object.getClass().getName(), "InventoryView", "Container") ? object : ((InventoryEvent)object).getView()));
+            final Method getTitle = view.getClass().getMethod("getTitle");
+            getTitle.setAccessible(true);
+            return (String) getTitle.invoke(view);
+        } catch (Exception e) {
+            ServerUtils.logSevere("{CompatUtils} An error has occurred with InventoryView#getTitle!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Attempts to get the InventoryType of the InventoryView.
+     * In API versions 1.21 (and above), InventoryView is an interface.
+     * In versions 1.20.6 and below, InventoryView is a class.
+     *
+     * @param object The object being referenced, this can be either a Player object, InventoryView object, or InventoryEvent object.
+     * @return The InventoryType from the object.
+     */
+    public static @Nonnull InventoryType getInventoryType(final @Nonnull Object object) {
+        try {
+            final Object view = (object instanceof Player ? getOpenInventory(object) : (StringUtils.containsIgnoreCase(object.getClass().getName(), "InventoryView", "Container") ? object : ((InventoryEvent)object).getView()));
+            final Method getType = view.getClass().getMethod("getType");
+            getType.setAccessible(true);
+            return (InventoryType) getType.invoke(view);
+        } catch (Exception e) {
+            ServerUtils.logSevere("{CompatUtils} An error has occurred with InventoryView#getType!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Attempts to get the Cursor ItemStack of the InventoryView.
+     * In API versions 1.21 (and above), InventoryView is an interface.
+     * In versions 1.20.6 and below, InventoryView is a class.
+     *
+     * @param object The object being referenced, this can be either a Player object, InventoryView object, or InventoryEvent object.
+     * @return The Cursor ItemStack from the object.
+     */
+    public static @Nonnull ItemStack getCursor(final @Nonnull Object object) {
+        try {
+            final Object view = (object instanceof Player ? getOpenInventory(object) : (StringUtils.containsIgnoreCase(object.getClass().getName(), "InventoryView", "Container") ? object : ((InventoryEvent)object).getView()));
+            final Method getCursor = view.getClass().getMethod("getCursor");
+            getCursor.setAccessible(true);
+            return (ItemStack) getCursor.invoke(view);
+        } catch (Exception e) {
+            ServerUtils.logSevere("{CompatUtils} An error has occurred with InventoryView#getCursor!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Attempts to set the Cursor ItemStack of the InventoryView.
+     * In API versions 1.21 (and above), InventoryView is an interface.
+     * In versions 1.20.6 and below, InventoryView is a class.
+     *
+     * @param object The object being referenced, this can be either a Player object, InventoryView object, or InventoryEvent object.
+     * @param itemStack The ItemStack to be set on the cursor.
+     */
+    public static void setCursor(final @Nonnull Object object, final @Nonnull ItemStack itemStack) {
+        try {
+            final Object view = (object instanceof Player ? getOpenInventory(object) : (StringUtils.containsIgnoreCase(object.getClass().getName(), "InventoryView", "Container") ? object : ((InventoryEvent)object).getView()));
+            final Method setCursor = view.getClass().getMethod("setCursor", ItemStack.class);
+            setCursor.setAccessible(true);
+            setCursor.invoke(view, itemStack);
+        } catch (Exception e) {
+            ServerUtils.logSevere("{CompatUtils} An error has occurred with InventoryView#setCursor!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Attempts to get the ItemStack of the InventoryView.
+     * In API versions 1.21 (and above), InventoryView is an interface.
+     * In versions 1.20.6 and below, InventoryView is a class.
+     *
+     * @param object The object being referenced, this can be either a Player object, InventoryView object, or InventoryEvent object.
+     * @param slot The slot of the ItemStack.
+     * @return The ItemStack from the object.
+     */
+    public static @Nonnull ItemStack getItem(final @Nonnull Object object, final int slot) {
+        try {
+            final Object view = (object instanceof Player ? getOpenInventory(object) : (StringUtils.containsIgnoreCase(object.getClass().getName(), "InventoryView", "Container") ? object : ((InventoryEvent)object).getView()));
+            final Method getCursor = view.getClass().getMethod("getItem", int.class);
+            getCursor.setAccessible(true);
+            return (ItemStack) getCursor.invoke(view, slot);
+        } catch (Exception e) {
+            ServerUtils.logSevere("{CompatUtils} An error has occurred with InventoryView#getItem!");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Attempts to set the ItemStack of the InventoryView.
+     * In API versions 1.21 (and above), InventoryView is an interface.
+     * In versions 1.20.6 and below, InventoryView is a class.
+     *
+     * @param object The object being referenced, this can be either a Player object, InventoryView object, or InventoryEvent object.
+     * @param itemStack The ItemStack to be set.
+     * @param slot The slot to set the ItemStack.
+     */
+    public static void setItem(final @Nonnull Object object, final @Nonnull ItemStack itemStack, final int slot) {
+        try {
+            final Object view = (object instanceof Player ? getOpenInventory(object) : (StringUtils.containsIgnoreCase(object.getClass().getName(), "InventoryView", "Container") ? object : ((InventoryEvent)object).getView()));
+            final Method setItem = view.getClass().getMethod("setItem", ItemStack.class, int.class);
+            setItem.setAccessible(true);
+            setItem.invoke(view, itemStack, slot);
+        } catch (Exception e) {
+            ServerUtils.logSevere("{CompatUtils} An error has occurred with InventoryView#setItem!");
+            throw new RuntimeException(e);
+        }
+    }
+}
