475f6ee313c57be5f88f314de324502ebf35be6a
RockinChaos <10971577+RockinChaos@users.noreply.github.com>
2026-01-24 14:30:13 -0800

    feat: Vault permissions hook

--- src/main/java/me/RockinChaos/core/utils/api/VaultAPI.java
+++ src/main/java/me/RockinChaos/core/utils/api/VaultAPI.java
@@ -20,7 +20,9 @@ package me.RockinChaos.core.utils.api;
 import me.RockinChaos.core.Core;
 import me.RockinChaos.core.utils.SchedulerUtils;
 import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
@@ -30,6 +32,7 @@ import javax.annotation.Nonnull;
 public class VaultAPI {
     private static VaultAPI vault;
     private Economy econ = null;
+    private Permission perms = null;
 
     /**
      * Creates a new VaultAPI instance.
@@ -37,6 +40,7 @@ public class VaultAPI {
     public VaultAPI() {
         if (Bukkit.getServer().getPluginManager().isPluginEnabled("Vault")) {
             this.setupEconomy();
+            this.setupPermissions();
         }
     }
 
@@ -74,6 +78,26 @@ public class VaultAPI {
     }
 
     /**
+     * Sets the Permission instance.
+     */
+    private void setupPermissions() {
+        try {
+            if (!Bukkit.getServer().getPluginManager().isPluginEnabled("Vault")) {
+                if (!Core.getCore().isStarted()) {
+                    SchedulerUtils.runLater(1L, this::setupPermissions);
+                }
+            } else {
+                RegisteredServiceProvider<Permission> rsp = Core.getCore().getPlugin().getServer().getServicesManager().getRegistration(Permission.class);
+                if (rsp == null) {
+                    return;
+                }
+                this.perms = rsp.getProvider();
+            }
+        } catch (Exception ignored) {
+        }
+    }
+
+    /**
      * Gets the Vault Economy instance.
      *
      * @return Gets the current economy instance.
@@ -83,12 +107,30 @@ public class VaultAPI {
     }
 
     /**
+     * Gets the Vault Permission instance.
+     *
+     * @return Gets the current permission instance.
+     */
+    public @Nonnull Permission getPermissions() {
+        return this.perms;
+    }
+
+    /**
      * Checks if Vault is enabled.
      *
      * @return If Vault is enabled.
      */
     public boolean vaultEnabled() {
         return Bukkit.getServer().getPluginManager().isPluginEnabled("Vault") && econ != null;
+    }
+
+    /**
+     * Checks if Vault permissions are enabled.
+     *
+     * @return If Vault permissions are enabled.
+     */
+    public boolean permissionsEnabled() {
+        return Bukkit.getServer().getPluginManager().isPluginEnabled("Vault") && perms != null;
     }
 
     /**
@@ -109,5 +151,34 @@ public class VaultAPI {
      */
     public void withdrawBalance(final @Nonnull Player player, final double cost) {
         this.econ.withdrawPlayer(player, cost);
+    }
+
+    /**
+     * Checks if an offline player has a specific permission.
+     *
+     * @param player     - The offline player to check.
+     * @param permission - The permission node to check.
+     * @return True if the player has the permission.
+     */
+    public boolean hasPermission(final @Nonnull OfflinePlayer player, final @Nonnull String permission) {
+        if (perms == null) {
+            return false;
+        }
+        return perms.playerHas(null, player, permission);
+    }
+
+    /**
+     * Checks if an offline player has a specific permission in a world.
+     *
+     * @param worldName  - The world name to check permission in.
+     * @param player     - The offline player to check.
+     * @param permission - The permission node to check.
+     * @return True if the player has the permission.
+     */
+    public boolean hasPermission(final @Nonnull String worldName, final @Nonnull OfflinePlayer player, final @Nonnull String permission) {
+        if (perms == null) {
+            return false;
+        }
+        return perms.playerHas(worldName, player, permission);
     }
 }
